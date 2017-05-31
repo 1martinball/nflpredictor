9c8dd77567e6d2338a482c20b54240f521ad560c
Martin Ball <1martinball@gmail.com>
2017-05-31 20:41:56 +0100

    MaBa: Summary page made clickable and updating local prediction

--- public/js/nfl-summary.js
+++ public/js/nfl-summary.js
@@ -7,8 +7,32 @@
 
 var predictionSummaryFixtures = null;
 
+let bindClickEvents = function() {
+
+    var $clickableElements = $("td");
+
+    $clickableElements.on("click", function(event){
+
+        var target = event.currentTarget.id;
+        var newPredictionForGame = target.split('-')[0];
+        var index = parseInt(target.split('-')[1]);
+        var predictionString = $('#predictionString').val();
+        var currentPredictionForGame = predictionString[index];
+
+        if(currentPredictionForGame !== newPredictionForGame) {
+            var newPredictionString = predictionString.substr(0,index) + newPredictionForGame + predictionString.substr(index+1);
+            $('#predictionString').val(newPredictionString);
+
+            $("#"+currentPredictionForGame+"-"+index).removeClass(currentPredictionForGame+"Pick").addClass(newPredictionForGame+"Pick");
+            $("#"+newPredictionForGame+"-"+index).addClass(newPredictionForGame+"Pick").removeClass(currentPredictionForGame+"Pick");
+        }
+    });
+
+}
 
 $(document).ready(function () {
+
+    bindClickEvents();
 
     if(predictionSummaryFixtures) {
         for(var i=0; i < predictionSummaryFixtures.length; i++){
@@ -16,7 +40,6 @@ $(document).ready(function () {
             $('img.home' + i).attr('src', 'images/teams/' + predictionSummaryFixtures[i].homeTeam.team + '_logo.svg');
         }
     }
-
 });
 
 

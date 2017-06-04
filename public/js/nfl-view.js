324dee7210f6df7433c302ec6391c7a5f0e82afd
Martin Ball <1martinball@gmail.com>
2017-06-04 22:29:16 +0100

    MaBa : Added ability to view and display results of a game

--- public/js/nfl-view.js
+++ public/js/nfl-view.js
@@ -6,6 +6,59 @@
 /////////////////////////////////////////////////////////
 
 
+let fetchResultTableData = function(game, week, season, players){
+
+    console.log("INFO: Calling getAllFixture service for : Week - " + week + " : Season - " + season);
+    $.ajax({
+        url: '/getAllFixtures',
+        data: "week=" + week + "&season=" + season,
+        success: function (result, status, req) {
+            console.log("DEBUG : Result returned from GET/getAllFixtures - " + JSON.stringify(result));
+            totalGames = result.totalFixtures;
+            createTable(game, players, result.fixtures, totalGames);
+            $('.page-title').slideUp('slow');
+        }
+    });
+
+}
+
+let createTable = function(game, players, fixtures, totalgames) {
+    console.log("INFO : About to create result table");
+    console.log("DEBUG : There are " + players.length + " players in this game");
+
+    for(var j = 0; j < totalgames; j++){
+        $('#result-table-body').append("<tr id='row-fixture-" + j + "'><td id='R-" + j + "' class='road-col text-center'></td><td id='T-" + j + "' class='text-col text-center'>@</td><td id='H-" + j + "' class='home-col  text-center'></td></tr>>");
+        $('#R-' + j).append("<img class='team-badge-smaller away" + j + "' type='image/svg+xml' src='/images/teams/" + fixtures[j].awayTeam.team + "_logo.svg' />");
+        $('#R-' + j).append(" : " + fixtures[j].awayTeam.score).addClass('text-col');
+        $('#H-' + j).append("<img class='team-badge-smaller home" + j + "' type='image/svg+xml' src='/images/teams/" + fixtures[j].homeTeam.team + "_logo.svg' />");
+        $('#H-' + j).append(" : " + fixtures[j].homeTeam.score).addClass('text-col');
+
+        for(var i = 0; i < players.length; i++) {
+            if (j == 0) {
+                $(".result-table-header-row").append("<th class='text-center'>" + players[i].name + "</th>");
+            }
+            $('#row-fixture-' + j).append("<td id='P" + i + "row" + j + "' class='player-col text-center'>");
+            if (players[i].prediction[j] !== 'T') {
+                $("#P" + i + "row" + j).append("<img class='team-badge-smaller' type='image/svg+xml' src='/images/teams/" + convertPlayerPrediction(players[i].prediction[j], fixtures[j]) + "' />");
+            } else {
+                $("#P" + i + "row" + j).append("TIE").addClass('text-col');
+            }
+        }
+    }
+    $('.game-view-table').slideDown('slow');
+    $('.game-view-table').removeClass('hide');
+}
+
+let convertPlayerPrediction = (prediction, fixture) => {
+    if(prediction === 'H') {
+        return fixture.homeTeam.team + "_logo.svg";
+    } else if(prediction === 'R') {
+        return fixture.awayTeam.team + "_logo.svg";
+    } else {
+        return 'TIE.jpg';
+    }
+}
+
 let bindClickEvents = function(){
 
 }
@@ -20,49 +73,12 @@ let bindChangeEvents = function(){
         season = existingGamesList.find(function(game){
             return game.name === currentGame;
         }).season;
+        var players = existingGamesList.find(function(game){
+            return game.name === currentGame;
+        }).players;
         $('.error').html("");
-        fetchResultTableData(currentGame, currentWeek, season);
-
-
-
+        fetchResultTableData(currentGame, currentWeek, season, players);
     });
-}
-
-let fetchResultTableData = function(game, week, season){
-
-    console.log("INFO : getPlayerForGameService : Attempting to retrieve list of players for this game - " + game);
-    $.ajax("/getPlayersForGame", {
-        method: 'GET',
-        data: "game="+game,
-        success: function (players, status, req) {
-            console.log("INFO : getPlayerForGameService : Ajax GET/getPlayerForGame returned successfully - " + players);
-            $('.message.error').html("");
-            console.log("INFO: Calling getAllFixture service for : Week - " + week + " : Season - " + season);
-            $.ajax({
-                url: '/getAllFixtures',
-                data: "week=" + week + "&season=" + season,
-                success: function (result, status, req) {
-                    console.log("DEBUG : Result returned from GET/getAllFixtures - " + JSON.stringify(result));
-                    totalGames = result.totalFixtures;
-                    createTable(players, result.fixtures, totalGames);
-                    $('.page-title').slideUp('slow');
-                }
-            });
-        },
-        error:  function() {
-            $('.message.error').html(result);
-            $('.message.error').removeClass('hide');
-            setTimeout(function () {
-                $('.message').html("");
-            }, 3000);
-            resetGame();
-            window.location("/");
-        }
-    });
-}
-
-let createTable = function(players, fixtures, totalgames) {
-    console.log("INFO : About to create result table");
 }
 
 let bindSubmitEvent = function(){
